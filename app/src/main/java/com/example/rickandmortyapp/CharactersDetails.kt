813a339767ca18d2123ed26d7b56e6c18dd4d501
Angelica García Núñez <angelica.garcia@gft.com>
2023-03-30 22:14:20 -0600

    cambios agregados, navegación entre vistas, navigation.

--- app/src/main/java/com/example/rickandmortyapp/CharactersDetails.kt
+++ app/src/main/java/com/example/rickandmortyapp/CharactersDetails.kt
@@ -1,12 +1,34 @@
 package com.example.rickandmortyapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.character_details.*
 
 class CharactersDetails : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.character_details, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_character_detail.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                findNavController().popBackStack()
+            }
+
+        })
+    }
 }
--- app/src/main/java/com/example/rickandmortyapp/CharactersList.kt
+++ app/src/main/java/com/example/rickandmortyapp/CharactersList.kt
@@ -1,20 +1,33 @@
 package com.example.rickandmortyapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.character_list.*
 
 class CharactersList : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    fun addActions(){
-        requireView().findViewById<ListView>(R.id.lv_Characters).setOnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.character_list, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_character_list.setOnClickListener {
             findNavController().navigate(R.id.action_charactersList_to_charactersDetails)
         }
-
     }
 }
